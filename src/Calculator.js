f513ec5986728f52b416b89d6ff50cff43024ee7
ArielCalisaya <calisayaariel62@gmail.com>
2020-05-29 00:36:48 -0400

    testing hooks and deployment funcionalities

---
+++ src/Calculator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Counter from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> 
+      </header>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
